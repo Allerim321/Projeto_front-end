e5fb2d8a74c0cdde487bc8f82ff3a32da7cd8f4f
Mirella <mirellatcg@gmail.com>
2023-11-28 10:41:23 -0300

    up

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,24 @@
+import { FormControl, FormGroup, FormLabel } from "react-bootstrap"
+import Conteudo from "../Components/Conteudo"
 import Header from "../Components/Header"
 
 function Login() {
     return (
         <>
-        <Header btn={true} />
+        <Header btn={false} />
+        <Conteudo>
+            <Form>
+                <FormGroup>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl type="email"
+                    placeholder="name@example.com"></FormControl>
+                </FormGroup>
+                <FormGroup>
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl type="password"></FormControl>
+                </FormGroup>
+            </Form>
+        </Conteudo>
         </>
     )
 }
