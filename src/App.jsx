a18bba377c23ba0ede321d13718e6beab79eb598
Mirella <mirellatcg@gmail.com>
2023-11-28 10:00:15 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Login from './Pages/Login'
+import Painel from './Pages/Painel'
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Login />} />
+      <Route path='/painel' element={<Painel />} />
     </Routes>
     </BrowserRouter>
   )
