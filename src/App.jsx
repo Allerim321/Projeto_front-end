f3da6dfe7bfc761782deeb5921e82b44cbef78c7
Mirella <mirellatcg@gmail.com>
2023-11-28 09:56:26 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Login from './Pages/Login'
+import { BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
-    <>
-    <Login />
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
