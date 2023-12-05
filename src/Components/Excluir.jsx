e34515d51da248657a295c0e291e904d890c3524
Mirella <mirellatcg@gmail.com>
2023-12-05 10:06:53 -0300

    up

--- src/Components/Excluir.jsx
+++ src/Components/Excluir.jsx
@@ -1,14 +1,16 @@
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
+import { useState } from 'react'
 
 function Excluir({id}){
+    const [show, setShow] = useState(true)
     return(
         <div>
             <span></span>
-            <Modal show={true}>
+            <Modal show={show} onHide={() => setShow(false)}>
                 <Modal.Header closeButton><h2>Excluir</h2></Modal.Header>
                 <Modal.Body><p>Deseja excluir o item?</p></Modal.Body>
-                <Modal.Footer><Button>Excluir</Button></Modal.Footer>
+                <Modal.Footer><Button class="btn btn-outline-danger">Excluir</Button></Modal.Footer>
             </Modal>
         </div>
     )
