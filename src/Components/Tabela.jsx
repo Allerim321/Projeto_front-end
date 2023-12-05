f516c9fc7a0376db42647040f4f6c588ca77e4bc
Mirella <mirellatcg@gmail.com>
2023-12-05 08:56:39 -0300

    up

--- src/Components/Tabela.jsx
+++ src/Components/Tabela.jsx
@@ -1,9 +1,13 @@
 import Table from 'react-bootstrap/Table'
 import Axios from 'axios'
 
+//criando uma array vazia para guardar os dados da tabela
+const[dados, setDados] = useState([])
+
+//utilizando a biblioteca axios, o método get fará uma requisição de dados
 Axios.get("https://apiaulas.thiagodev502.repl.co/funcionarios").then(
     (resposta) =>{
-        console.log(resposta.data)
+        setDados(resposta.data)
     }).catch(
         (error) =>{
             console.log(error)
@@ -20,8 +24,16 @@ function Tabela(){
                         <th>Ação</th>
                     </tr>
                 </thead>
+                {/* passando os dados requeridos pela biblioteca Axios para a tabela */}
                 <tbody>
-                    
+                    {dados.map((item, key) => (
+                        <tr>
+                            <td>{item.id}</td>
+                            <td>{item.nome}</td>
+                            <td>{item.cargo}</td>
+                            <td></td>
+                        </tr>
+                   ))}
                 </tbody>
             </Table>
         </>
