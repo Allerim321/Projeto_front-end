51c3aae3134811a785db24e690c81ebba92804f2
Mirella <mirellatcg@gmail.com>
2023-12-05 10:00:42 -0300

    up

--- src/Components/Excluir.jsx
+++ src/Components/Excluir.jsx
@@ -1,6 +1,16 @@
-function Excluir(){
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+
+function Excluir({id}){
     return(
-        <div>Excluir</div>
+        <div>
+            <span></span>
+            <Modal show={true}>
+                <Modal.Header closeButton><h2>Excluir</h2></Modal.Header>
+                <Modal.Body><p>Deseja excluir o item?</p></Modal.Body>
+                <Modal.Footer><Button>Excluir</Button></Modal.Footer>
+            </Modal>
+        </div>
     )
 }
 
