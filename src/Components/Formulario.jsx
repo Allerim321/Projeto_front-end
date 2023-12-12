419d05412fbd556c2bf1ba34695ac95cb2143238
Mirella <mirellatcg@gmail.com>
2023-12-12 13:37:17 -0300

    elementos formulario

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -7,6 +7,21 @@ function Formulario(params) {
                 <Form.Label>Nome:</Form.Label>
                 <Form.Control type='text'></Form.Control>
             </Form.Group>
+            <Form.Group>
+                <Form.Label>Cargo:</Form.Label>
+                <Form.Control type='text'></Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Departamento:</Form.Label>
+                <Form.Control type='text'></Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Salário:</Form.Label>
+                <Form.Control type='text'></Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Control type='submit'></Form.Control>
+            </Form.Group>
         </Form>
     )
 }
